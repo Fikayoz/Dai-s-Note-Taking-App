1eb6d55478ad1335ba6ed9f632bb64fc1f530204
Fikayo <Fikayoz@gmail.com>
2025-08-17 14:07:16 +0900

    Added load note data functionality (loads on web page load)

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn =  document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
 function updateStorage(){
     localStorage.setItem("notes", notesContainer.innerHTML);
 }
@@ -24,8 +28,10 @@ notesContainer.addEventListener("click", function(e){
         notes = document.querySelectorAll(".input-box"); 
         notes.forEach(nt => {
             nt.onkeyup = function(){
-                updateStorage();
+                updateStorage();    
             }
         })
     }
 })
+
+showNotes();
